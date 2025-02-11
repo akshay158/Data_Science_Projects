0537f0dfd98b9c6bb73621413125fb4cfbbdf449
akshay158 <64063846+akshay158@users.noreply.github.com>
2025-02-11 14:08:23 +0530

    Delete SQL Project directory

--- SQL Project/Task 6.sql
+++
@@ -1,6 +0,0 @@
-with genderSales_monthly as 
-(select Gender, date_format(Date, '%m-%Y') as Months, round(sum(Total),2) as total_sales
-from walmartsales 
-group by Gender, Months
-order by Months)
-select * from genderSales_monthly
